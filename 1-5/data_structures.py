c69bd6f1fecc2bfd2e025ba1bfc991543a02d54c
Zeel Patel <99717340+byZeel@users.noreply.github.com>
2026-04-13 14:00:31 +0530

    Add files via upload

---
+++ 1-5/data_structures.py
@@ -0,0 +1,27 @@
+# n=[1,2,3]
+# n.append(6)
+# print(n)
+#[1, 2, 3, 6]
+
+# n=(1,2,3)
+#     print(n)
+# # (1, 2, 3)
+
+# n={5,5,1,2,3,4,3}
+# print(n)
+# {1, 2, 3, 4, 5}
+
+# n=(1,2,3)
+# n += (6,)
+# print(n)
+
+# n={1,2,3}
+# n.add(4)
+# print(n)
+
+# std={"name":"Zeel","age":"22"}
+# print(std["name"])
+# print(std["age"])
+
+s={"a":21,"b":"zeel","c":"std"}
+print(s["a"],s["b"],s["c"])
