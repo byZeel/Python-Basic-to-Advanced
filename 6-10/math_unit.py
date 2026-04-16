05ff0a3089d12e45f2d15cae00d53e09d06f3d65
Zeel Patel <99717340+byZeel@users.noreply.github.com>
2026-04-16 08:42:14 +0530

    Add files via upload

---
+++ 6-10/math_unit.py
@@ -0,0 +1,20 @@
+
+#*  arithmetic = + addition
+#*               - substraction
+#*               * multiplication
+#*               / division
+#*              // integer division
+#*               % remainder
+
+def a(x,y):
+    return(x+y)
+def s(x,y):
+    return(x-y)
+def m(x,y):
+    return x*y
+def d(x,y):
+    return x/y
+def id(x,y):
+    return x//y
+def r(x,y):
+    return x%y
