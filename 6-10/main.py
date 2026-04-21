3931f95b22061bfaef28e23dcc46b242bf5db997
Zeel Patel <99717340+byZeel@users.noreply.github.com>
2026-04-21 19:20:30 +0530

    Add files via upload

---
+++ 6-10/main.py
@@ -0,0 +1,17 @@
+# from Calculator.add import add
+# from Calculator.sub import sub
+
+# print(add(10,12))
+# print(sub(10,12))
+
+# from Calculator import add as a
+# from Calculator import sub as s
+
+# print(a.add(10,12))
+# print(s.sub(10,12))
+
+from Calculator import add
+from Calculator import sub
+
+print(add.add(10,12))
+print(sub.sub(10,12))
